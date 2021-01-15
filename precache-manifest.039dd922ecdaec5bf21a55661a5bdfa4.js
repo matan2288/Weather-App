9394366302060d565ff786c88648f64468c8ad72
MatanGitTest <matan2288@gmail.com>
2021-01-15 22:57:11 +0200

    Updates

---
+++ precache-manifest.039dd922ecdaec5bf21a55661a5bdfa4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a258f718715242729cc7dfe83475c7d9",
+    "url": "/Weather-App/index.html"
+  },
+  {
+    "revision": "353de162b90e57adfb0e",
+    "url": "/Weather-App/static/css/main.614b5249.chunk.css"
+  },
+  {
+    "revision": "43fc1e96b9b502bd22db",
+    "url": "/Weather-App/static/js/2.a6674158.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Weather-App/static/js/2.a6674158.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "353de162b90e57adfb0e",
+    "url": "/Weather-App/static/js/main.3340c18d.chunk.js"
+  },
+  {
+    "revision": "5a89754edee3862e9a0e",
+    "url": "/Weather-App/static/js/runtime-main.49cbaed4.js"
+  }
+]);
